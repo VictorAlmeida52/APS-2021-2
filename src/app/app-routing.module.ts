437cdc5fe5cee936960abf79d8a07b87d2dcd2ba
Victor Almeida <victor.almeida@selens.com.br>
2021-10-03 22:30:15 -0300

    Implementa guarda de rota em /queimadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { WeatherPageComponent } from './weather-page/weather-page.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { QueimadasPageComponent } from './queimadas-page/queimadas-page.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'weather', component: WeatherPageComponent },
   { path: 'traffic', component: SideMenuComponent },
-  { path: 'queimadas', component: QueimadasPageComponent },
+  { path: 'queimadas', component: QueimadasPageComponent, canActivate: [AuthGuardService] },
   { path: 'about', component: SideMenuComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'queimadas', component: QueimadasPageComponent },
